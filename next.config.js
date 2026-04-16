006f1aff21c2beb5b208ed7b897c008ff69d4222
Greg Polk <navydudeintx@gmail.com>
2026-04-16 15:16:32 -0500

    Disable caching for page responses

--- next.config.js
+++ next.config.js
@@ -9,6 +9,29 @@ const nextConfig = {
     ];
   },
 
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/:path((?!_next/static|_next/image|favicon.ico|icon.png|android-chrome-192x192.png|manifest.json).*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+
   devIndicators: false,
 };
 
